4dc0ae8d2324d5b91e233feb28e8576eea6946e5
rinick <rinick@gmail.com>
2016-10-08 16:20:52 +0800

    disable gpu by default

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# 安装依赖包
-sudo apt-get update
-sudo apt-get -y install build-essential git cmake libprotobuf-dev protobuf-compiler curl libreadline-dev
-
 # 安装Dart，( 因为官方下载国内用不了，我把安装包复制在github上 )
 wget https://github.com/rinick/dart_sdk/releases/download/1.19.1/dart_1.19.1-1_amd64.deb
 sudo dpkg -i --force-all dart_1.19.1-1_amd64.deb
@@ -21,9 +17,10 @@ luarocks install loadcaffe
 
 # 安装Neural Style Server
 git clone https://github.com/rinick/neural-style-server.git --recursive
-sh ./neural-style-server/neural-style/models/download_models.sh
+cd neural-style-server/neural-style
+sh ./models/download_models.sh
 
-cd neural-style-server
+cd ..
 
 /usr/lib/dart/bin/pub get
 /usr/lib/dart/bin/pub build
