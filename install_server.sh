0e084a4c3cc223fb20793af739cc6895b231347d
rinick <rinick@gmail.com>
2016-10-09 23:20:22 -0700

    create pub cache before other tasks

--- install_server.sh
+++ install_server.sh
@@ -5,16 +5,6 @@
 curl -O -J -L https://github.com/rinick/sdk/releases/download/1.20.0-dev.10.1/dart_1.20.0-dev.10.1-1_amd64.deb
 sudo dpkg -i --force-all dart_1.20.0-dev.10.1-1_amd64.deb
 
-# install loadcaffee
-
-luarocks install loadcaffe
-
-# install Neural Style Server
-
-git clone https://github.com/rinick/neural-style-server.git --recursive
-cd neural-style-server/neural-style
-sh ./models/download_models.sh
-
 # a work around for the dart pub issue in windows 10 linux subsystem
 
 mkdir -p ~/.pub-cache/hosted/pub.dartlang.org
@@ -33,14 +23,29 @@ tar -xvf 0.9.3.tar.gz -C ~/.pub-cache/hosted/pub.dartlang.org/mime-0.9.3
 tar -xvf 1.3.9.tar.gz -C ~/.pub-cache/hosted/pub.dartlang.org/path-1.3.9
 tar -xvf 0.10.0%2B2.tar.gz -C ~/.pub-cache/hosted/pub.dartlang.org/browser-0.10.0+2
 
-cd neural-style-server
-
-/usr/lib/dart/bin/pub get --offline
-
-/usr/lib/dart/bin/pub build
-
 rm dart_1.20.0-dev.10.1-1_amd64.deb
 rm 0.9.6.tar.gz
 rm 0.9.3.tar.gz
 rm 1.3.9.tar.gz
 rm 0.10.0%2B2.tar.gz
+
+
+
+# install loadcaffee
+
+luarocks install loadcaffe
+
+# install Neural Style Server
+
+git clone https://github.com/rinick/neural-style-server.git --recursive
+cd neural-style-server/neural-style
+sh ./models/download_models.sh
+
+cd ..
+
+# compile dart code
+
+/usr/lib/dart/bin/pub get --offline
+
+/usr/lib/dart/bin/pub build
+
